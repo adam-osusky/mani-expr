2b69be6fb8dbf5294e5d51dda83be458dc569709
Adam Osusky <adam.osusky02@gmail.com>
2022-10-06 13:53:27 +0200

    Improve csv reader

--- cviceni1/main.cpp
+++ cviceni1/main.cpp
@@ -3,14 +3,15 @@
 #include <string>
 #include <vector>
 
-namespace Tutorial1{
-    constexpr char separator = ';';
-    void loadLine(std::vector<std::vector<std::string>> &db, std::string &line){
+namespace Tutorial1 {
+    constexpr char separator = ',';
+
+    void loadLine(std::vector<std::vector<std::string>> &db, std::string &line) {
         std::vector<std::string> dbLine;
-        int oldPosition = 0;
-        for (int currentPosition = 0; currentPosition < line.size(); ++currentPosition) {
-            if (line[currentPosition] == separator){
-                dbLine.push_back(line.substr(oldPosition, currentPosition-oldPosition));
+        std::size_t oldPosition = 0;
+        for (std::size_t currentPosition = 0; currentPosition < line.size(); ++currentPosition) {
+            if (line[currentPosition] == separator) {
+                dbLine.push_back(line.substr(oldPosition, currentPosition - oldPosition));
                 oldPosition = currentPosition + 1;
             }
         }
@@ -19,22 +20,35 @@ namespace Tutorial1{
 }
 
 int main(int argc, char * * argv) {
-//    for (int i = 0; i < argc; ++i) {
-//        std::cout << i << ": \"" << argv[i] << "\"" << std::endl;
-//    }
+    for (int i = 0; i < argc; ++i) {
+        std::cout << i << ": \"" << argv[i] << "\"" << std::endl;
+    }
+
+    if (argc < 2) {
+        std::cout << "Missing file name" << std::endl;
+        return 8;
+    }
 
     std::ifstream is(argv[1]);
     if (!is.good()) {
         std::cout << "File argument is not okay" << std::endl;
+        return 8;
     }
+
     std::string x;
     std::vector<std::vector<std::string>> db;
 
-    while (!is.eof()) {
+    for(;;) {
         std::getline(is, x);
         if (is.good()) {
             //std::cout << "\"" << x << "\"" << std::endl;
             Tutorial1::loadLine(db, x);
+        }
+        if (is.fail()) {
+            if (is.eof())
+                break;
+            std::cout << "Cannot read from the file" << std::endl;
+            return 8;
         }
     }
 
@@ -48,6 +62,8 @@ int main(int argc, char * * argv) {
     return 0;
 }
 
+
 // std::string x
 // std::ifstream is(file)
 // std::getline(is ,x) is.good() fail eof
+// emplace_back std::ptrdiff_t signed size t
